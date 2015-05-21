89e4aa103c81643727725f1a14f6809724d5a497
Édouard Touraile <edouard.touraille@gmail.com>
2015-05-21 16:27:39 +0200

    Edit Listen AMQShell to allow timeout exception

--- src/Resource/Shell/shell.php
+++ src/Resource/Shell/shell.php
@@ -13,31 +13,43 @@ define('AMQP_DEBUG', true);
 
 class ReceiverCommand {
 
+    protected $wait = false;
+
     public function execute() {
         
-        //listen to the brocker
-        $connection = new AMQPConnection('objetspartages.org', 5672, 'toto', 'toto', 'toto',
-        false, 'AMQPLAIN',null,'en_US', 3, 3, null, false, 2);
-         $channel = $connection->channel();
-         
+        while(true){
+            //listen to the brocker
+        try{
+            $connection = new AMQPConnection('objetspartages.org', 5672, 'toto', 'toto', 'toto',
+            false, 'AMQPLAIN',null,'en_US', 3, 3, null, false, 2);
+             $channel = $connection->channel();
+             
+            $channel->exchange_declare('indexing', 'direct', false, false, false);
 
-        $channel->exchange_declare('indexing', 'direct', false, false, false);
+            list($queue_name, ,) = $channel->queue_declare("", false, false, true, false);
 
-        list($queue_name, ,) = $channel->queue_declare("", false, false, true, false);
-
-                        
-        
-        $channel->queue_bind($queue_name, 'indexing', 'index');
+                            
+            
+            $channel->queue_bind($queue_name, 'indexing', 'index');
 
 
-        $channel->basic_consume($queue_name, '', false, true, false, false, array($this, 'callBack'));
+            $channel->basic_consume($queue_name, '', false, true, false, false, array($this, 'callBack'));
 
-        while(count($channel->callbacks)) {
-               $channel->wait();
+            $this->wait = false;
+            while(count($channel->callbacks)) {
+                   $channel->wait();
+            }
+
+
+            $channel->close();
+            $connection->close(); 
         }
-
-        $channel->close();
-        $connection->close(); 
+         catch(\Exception $e){
+            $this->wait();
+            echo $e->getMessage();
+            var_dump($e->getTrace());
+         } 
+        }
     }
 
     public function callBack($msg){
@@ -47,6 +59,17 @@ class ReceiverCommand {
         $elastic = new \Resource\Bundle\UserBundle\Service\Elastic();
         $return = $elastic->index('resource','hashtag',$data);
         echo $return;
+    }
+
+    public function wait(){
+        if(false === $this->wait){
+            sleep(10);
+            $this->wait = 10;
+        }else{
+            $this->wait = $this->wait/2;
+            sleep($this->wait);
+        }
+    
     }
 }
 $shell = new ReceiverCommand();
