e7bb18ed43cb160bed480a335f58db02138a080d
Édouard Touraile <edouard.touraille@gmail.com>
2015-06-11 13:10:50 +0200

    API Search Bugfix

--- src/Resource/Bundle/UserBundle/Controller/SearchController.php
+++ src/Resource/Bundle/UserBundle/Controller/SearchController.php
@@ -5,7 +5,7 @@ namespace Resource\Bundle\UserBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Resource\Bundle\UserBundle\Document\Search;
 use Symfony\Component\HttpFoundation\Response;
-use Resource\Bundle\UserController\Service\Elastic;
+use Resource\Bundle\UserBundle\Service\Elastic;
 
 class SearchController extends Controller {
 
