2ec814638834d2e12beb3ab928f4515cbee7e312
Édouard Touraile <edouard.touraille@gmail.com>
2015-07-25 00:05:41 +0200

    co

--- src/Resource/Bundle/UserBundle/Controller/MessagingController.php
+++ src/Resource/Bundle/UserBundle/Controller/MessagingController.php
@@ -136,7 +136,7 @@ class MessagingController extends Controller {
 
          // Messaging send the message to device ...
          
-         $this->get('notification')->send($userTo,$content);
+         $this->get('notification')->send($userTo,$content, array('id'=>$from));
          return (new Response())->setContent(json_encode(array('success'=>true)));
     
     }
